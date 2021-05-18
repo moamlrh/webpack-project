40481f527854c5a48d201019f6cdd15fc09ed87d
moamlrh <moamll288@gmail,com>
2021-05-19 02:10:59 +0300

    add babel-loader and @babel/core and preset-env with configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // go ahead
+const x = () => console.log("x");
+const c = () => console.log("c");
+
+x();
+c();
+
+const a = {
+  a: "1",
+  b: "31",
+  c: 12,
+};
+
+const b = {
+  ...a,
+  b: "2",
+};
+
+console.log(a);
+console.log(b);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
 const path = require("path");
 
-module.exports = () => {
-  const config = {};
+const join = (...paths) => path.join(__dirname, ...paths);
+
+module.exports = (env) => {
+  const isDevelopment = env.mode === "development";
+  const isProduction = env.mode === "production";
+
+  const config = {
+    devtool: false,
+    devServer: {
+      contentBase: join("dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/i,
+          include: join("src"),
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+      ],
+    },
+  };
 
   return config;
 };
