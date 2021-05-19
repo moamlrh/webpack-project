e3697a18e2b9f155487311373968bfad2df15b13
moamlrh <moamll288@gmail,com>
2021-05-19 15:57:17 +0300

    add index.htlm to src and fix hot replacement reolding if u change in css or sass will reload by changing the devServer contentBase to src and change the html webpack plugin template to serve from src/index.html

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ input.type = "text";
 
 const img = new Image(); // 37kb
 img.src = image;
+img.width = 200;
 
 document.body.appendChild(img);
 document.body.appendChild(input);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const MiniCssExtractPuglins = require('mini-css-extract-plugin');
-const HtmlWebapckPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const MiniCssExtractPuglins = require("mini-css-extract-plugin");
+const HtmlWebapckPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 const join = (...paths) => path.join(__dirname, ...paths);
 
 module.exports = (env) => {
-  const isDevelopment = env.mode === 'development';
-  const isProduction = env.mode === 'production';
+  const isDevelopment = env.mode === "development";
+  const isProduction = env.mode === "production";
 
   const config = {
-    mode: isDevelopment ? 'development' : 'production',
-    devtool: isDevelopment ? 'eval' : 'source-map',
-    target: isDevelopment ? 'web' : 'browserslist',
+    mode: isDevelopment ? "development" : "production",
+    devtool: isDevelopment ? "eval" : "source-map",
+    target: isDevelopment ? "web" : "browserslist",
     devServer: {
-      contentBase: join('dist'),
+      contentBase: join("src"),
       open: true,
       hot: true,
     },
@@ -22,31 +22,32 @@ module.exports = (env) => {
       rules: [
         {
           test: /\.js$/i,
-          include: join('src'),
+          include: join("src"),
           exclude: /node_modules/,
-          use: ['babel-loader'],
+          use: ["babel-loader"],
         },
         {
           test: /\.(css|sass|scss)$/i, // to check all files css tyeps
-          use: [MiniCssExtractPuglins.loader, 'css-loader', 'sass-loader'],
+          use: [MiniCssExtractPuglins.loader, "css-loader", "sass-loader"],
         },
         {
           test: /\.(png|jpg|jpeg|svg)/i,
-          use: 'url-loader',
+          use: "url-loader",
         },
       ],
     },
     plugins: [
       new HtmlWebapckPlugin({
-        inject: 'body',
-        title: 'Webpack project',
+        inject: "body",
+        title: "Webpack project",
+        template: join("src", "index.html"),
       }),
       new MiniCssExtractPuglins({
-        filename: '[name].[hash:5].css',
+        filename: "name.css",
       }),
       new webpack.HotModuleReplacementPlugin(),
     ],
   };
-  console.log(config.mode);
+  console.log("the mode is  ==>> ", config.mode);
   return config;
 };
